a9c28e02106ea9087c0610ea60383807bc23c4c1
keenanguyparker@gmail.com <valenciacurbello@Valencias-MacBook-Pro.local>
2016-06-02 20:16:15 -0700

    add contact page, add thank-you email response panel, add thank-you email action, install sendgrid plugin

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,4 +11,14 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(:from => @email,
+  		:to => 'keenanguyparker@gmail.com',
+  		:subject => "A new contact-form message from #{@name}",
+  		:body => @message).deliver_now
+  end
 end
